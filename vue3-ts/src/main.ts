fe5329466100fd8882a6507857dc6f69385eaaf9
Mr.win11 <463736788@qq.com>
2026-01-16 20:05:03 +0800

    vue3-ts初始化

--- vue3-ts/src/main.ts
+++ vue3-ts/src/main.ts
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'dayjs/locale/zh-cn'
+import DevUI from 'vue-devui';
+import 'vue-devui/style.css';
+import '@devui-design/icons/icomoon/devui-icon.css';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(DevUI);
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
