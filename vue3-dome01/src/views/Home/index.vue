fa5ef5768c3a42dd01704826ae6fe6367a6eb2bb
Xuan.win10 <wangxuan1734@gmail.com>
2026-01-22 16:11:42 +0800

    input

--- vue3-dome01/src/views/Home/index.vue
+++ vue3-dome01/src/views/Home/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <el-input v-model="formItems.name" placeholder="请输入姓名" type="textarea" @keydown="submitForm" />
+  </div>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue'
 import { useWebSocket } from '@vueuse/core'
 
 const ws = import.meta.env.VITE_APP_WS_URL
@@ -28,6 +31,34 @@ watch(
     }
   }
 )
+
+const submitForm = (event) => {
+  if (event.key === 'Enter' || event.keyCode === 13) {
+    // 检测组合键
+    const isCombinationKey = event.ctrlKey || event.metaKey || event.shiftKey
+    console.log('isCombinationKey', isCombinationKey)
+    if (!isCombinationKey) {
+      // 纯 Enter 键：提交消息
+      event.preventDefault()
+      event.stopPropagation()
+      submitMessage()
+    }
+    // 组合键 + Enter：允许默认换行行为
+  }
+}
+
+// 辅助函数
+const submitMessage = () => {
+  // 可以添加发送成功的视觉反馈
+  console.log('消息已发送')
+  send(
+    JSON.stringify({
+      event: 'message',
+      data: formItems.value.name,
+      timestamp: Date.now()
+    })
+  )
+}
 
 watch(
   () => data.value,
