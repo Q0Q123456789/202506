14e31d8f3576c85bfe8e89078525ca35ec1fb08c
Mr.Think <463736788@qq.com>
2026-03-13 12:22:57 +0800

    添加地图事件

--- vue3-ts/src/App.vue
+++ vue3-ts/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="map" ref="map">
+  <div id="map">
     <!--    111-->
     <!--    <router-view></router-view>-->
   </div>
@@ -11,28 +11,35 @@ import View from 'ol/View'
 import TileLayer from 'ol/layer/Tile'
 import XYZ from 'ol/source/XYZ'
 
-import { onMounted, useTemplateRef } from 'vue'
-import { fromLonLat } from 'ol/proj'
+import { onMounted } from 'vue'
+import { fromLonLat, toLonLat } from 'ol/proj'
 
-const mapRef = useTemplateRef('map')
+const token = import.meta.env.VITE_MAP_KEY
 
 onMounted(() => {
   const map = new Map({
     // target指向DOM元素
-    target: mapRef.value,
+    target: 'map',
     // 配置图层
     layers: [
       new TileLayer({
         // source: new OSM(), // 使用OpenStreetMap数据源
         source: new XYZ({
-          url: `http://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${import.meta.env.VITE_MAP_KEY}`,
+          url: `http://t0.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${token}`,
           attributions: '© 天地图',
         }),
       }),
       new TileLayer({
         // source: new OSM(), // 使用OpenStreetMap数据源
         source: new XYZ({
-          url: `http://t0.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${import.meta.env.VITE_MAP_KEY}`,
+          url: `http://t0.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${token}`,
+          attributions: '© 天地图',
+        }),
+      }),
+      new TileLayer({
+        // source: new OSM(), // 使用OpenStreetMap数据源
+        source: new XYZ({
+          url: `http://t0.tianditu.gov.cn/ibo_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ibo&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${token}`,
           attributions: '© 天地图',
         }),
       }),
@@ -47,7 +54,19 @@ onMounted(() => {
     }),
   })
 
-  console.log(map)
+  map.on('singleclick', (e) => {
+    console.log(e.coordinate)
+    console.log(toLonLat(e.coordinate))
+    console.log('当前缩放级别:', map.getView().getZoom())
+  })
+
+  // 滚动事件处理
+  map.on('change', (e) => {
+    console.log('滚动事件:', e)
+    // console.log('滚动方向:', e.deltaY > 0 ? '向下滚动（缩小）' : '向上滚动（放大）')
+    // console.log('滚动速度:', e.deltaY)
+    console.log('当前缩放级别:', map.getView().getZoom())
+  })
 })
 </script>
 <style scoped>
