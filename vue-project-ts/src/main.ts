ec8bce0b041c264b0f6c29a70b159ae4938f992e
Mr.Think <463736788@qq.com>
2026-04-09 14:25:42 +0800

    初始化配置

--- vue-project-ts/src/main.ts
+++ vue-project-ts/src/main.ts
@@ -3,10 +3,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+
+import './style/index.css'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus, { locale: zhCn })
 
 app.mount('#app')
--- vue-project-ts/vite.config.ts
+++ vue-project-ts/vite.config.ts
@@ -1,20 +1,45 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
+import autoprefixer from "autoprefixer";
+import postcssImport from "postcss-import";
+import postcssNested from "postcss-nested";
+import postcssPxtorem from "postcss-pxtorem";
+import postcssUrl from "postcss-url";
+import postcssTailwindcss from "@tailwindcss/postcss";
+import tailwindcss from "@tailwindcss/vite";
+import VueRouter from "vue-router/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueRouter({
+      /* options */
+    }),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(),
+        postcssNested(),
+        postcssImport(),
+        postcssPxtorem(),
+        postcssUrl(),
+        postcssTailwindcss(),
+        // 其他插件...
+      ],
+    },
+  },
+});
