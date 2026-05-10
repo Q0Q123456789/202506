d0276bf50bbd3f1c052f24384b210cf7fe283ff6
Mr.win11 <wx463736788@outlook.com>
2026-05-10 18:34:55 +0800

    清除久项目

--- electronDome01/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  sendMessage: (channel, data) => ipcRenderer.send(channel, data),
-  receiveMessage: (channel, func) =>
-    ipcRenderer.on(channel, (event, ...args) => func(...args)),
-});
